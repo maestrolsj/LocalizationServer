4633b394eaca034af6d65dc30b2949b9de9c4591
yyhan0915 <yyhan2059@hanmail.net>
2021-01-27 22:33:07 +0100

    feat : build를 로컬에서 하도록 함

---
+++ client/build/precache-manifest.8d80c4b59c4b687a94391ed66a262c6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b52826d68acb70cc8326b78e44666690",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41a920574c4f9ed89e56",
+    "url": "/static/css/main.10b87fb2.chunk.css"
+  },
+  {
+    "revision": "690124496778a70eccf7",
+    "url": "/static/js/2.2a45e1eb.chunk.js"
+  },
+  {
+    "revision": "f2905d63ccbced0ce9f2af1c213b8895",
+    "url": "/static/js/2.2a45e1eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a920574c4f9ed89e56",
+    "url": "/static/js/main.8a2ed49e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
