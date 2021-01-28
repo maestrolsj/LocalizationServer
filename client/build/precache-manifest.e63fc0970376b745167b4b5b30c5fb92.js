65cdf3d4f3b250d68e78d6130f54236a7d78000c
yyhan0915 <yyhan2059@hanmail.net>
2021-01-28 03:18:49 +0100

    build

---
+++ client/build/precache-manifest.e63fc0970376b745167b4b5b30c5fb92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a208023aaf2a5e1bd9c348dbe47b9e92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddd411839a3aad5b4ac4",
+    "url": "/static/css/main.10b87fb2.chunk.css"
+  },
+  {
+    "revision": "4f981c4feb16109d1a6d",
+    "url": "/static/js/2.dbde207f.chunk.js"
+  },
+  {
+    "revision": "f2905d63ccbced0ce9f2af1c213b8895",
+    "url": "/static/js/2.dbde207f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddd411839a3aad5b4ac4",
+    "url": "/static/js/main.f448e0c5.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
